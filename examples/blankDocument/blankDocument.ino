b7ccc1055dea1f36b25c27a5499ef5e0b5032cf2
Alex Walker <alex@decoded.co>
2015-02-18 11:00:17 +0000

    Update blankDocument.ino
    Removing headers and working UID

--- examples/blankDocument/blankDocument.ino
+++ examples/blankDocument/blankDocument.ino
@@ -1,12 +1,5 @@
-#include <DHT.h> //temperature and humidity library
-#include <Decoded.h> //Our library
-#include <Servo.h> //Servo motor library
-#include <Ethernet.h> //internet library
-#include <SPI.h> //communication library
-#include <SoftwareSerial.h> //talk with computer
-
 //this is your specific end point.
-  String url= "/api/?uid=8a7827b9813f7a054484109a28905793&trigger=t8myh&value=";  
+  String url= "/api/?uid=[YOUR ID]&trigger=t8myh&value=";  
 //this is to identify your computer
   byte computer = 0xAD; //change to your choice...
 //this is the object that handles all communication with the arduino
